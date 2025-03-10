6e6b29fd2e6569c260b8637b7d651b0f6ad8de80
호정 <w2314@e-mirim.hs.kr>
2025-03-11 00:37:34 +0900

    ✨ : 기본 API 라우트와 404 오류 페이지 처리 추가

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const server = require('http').createServer(app);
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send("Welcome to the server! Use /api to get data.");
+});
+
+app.get('/api', (req, res) => {
+  res.send({ message: "hello from the server!" });
+});
+
+app.use((req, res) => {
+  res.status(404).send("404 Not Found: The requested resource does not exist.");
+});
+
+server.listen(8080, () => {
+  console.log('server is running on http://localhost:8080');
+});
